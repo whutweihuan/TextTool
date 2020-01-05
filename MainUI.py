67d39193c30cd07add2de9b2918b0a4336a8c108
weihuan <1548710086@qq.com>
2020-01-05 21:42:25 +0800

    initial

--- MainUI.py
+++ MainUI.py
@@ -4,30 +4,65 @@
  date: 2020/1/5  19:02
 """
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QDesktopWidget,QToolTip)
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, \
+    QDesktopWidget, QToolTip, QPushButton, QMessageBox,QMainWindow,QAction,qApp
+from PyQt5.QtGui import QIcon, QFont
 
 
-class MainUI(QWidget):
+class MainUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         self.resize(800, 500)
-        # screen = QDesktopWidget.screenGeometry()
-        # screenSize = self.geometry()
-        # win.move((screen.width() - screenSize.width()) / 2,
-        #          (screen.height() - screenSize.height()) / 2)
+        QToolTip.setFont(QFont('SansSerif', 10))
         self.setWindowTitle("文本工具")
         self.setWindowIcon(QIcon('res/main_ui_title_icon.png'))
+        self.setToolTip('This is a <b> QWidget <b/> widget')
+
+        # 活动
+        exitAct = QAction(QIcon('res/main_ui_title_icon.png'),'&Exit',self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        # 状态栏
+        self.statusBar().showMessage('Ready')
+
+        # 菜单
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        # 按钮
+        btn = QPushButton('确认', self)
+        btn.setToolTip('这是一个按钮')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+        btn.clicked.connect(QApplication.instance().quit)
+        self.center()
+
+    # 退出二次确定
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, '退出', '确认退出吗?',
+                                     QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    # 居中
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = MainUI()
-
     win.show()
-
     sys.exit(app.exec())
